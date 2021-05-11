1541194128c2ba2b7a91fcc43211971861990f91
binhdao29 <binhchan0293@gmail.com>
2021-05-12 00:26:09 +0700

    menu-small visible

--- src/scripts/scripts.js
+++ src/scripts/scripts.js
@@ -1,7 +1,7 @@
-const btnDrawer = document.querySelector('.button-drawer');
-const menuDrawer = document.querySelector('.menu-drawer');
+const btnDrawer = document.querySelector('.button__drawer');
+const menuDrawer = document.querySelector('.menu__drawer');
 const btnClose = document.querySelector('.button-close');
-const menuMain = document.querySelector('.menu-drawer__main');
+const contentMenu = document.querySelector('.content');
 
 const menuTransparent = document.querySelector('.header--transparent');
 const btnOpenSearch = document.querySelector('.button--search');
@@ -9,24 +9,32 @@ const formSearch = document.querySelector('.search');
 const btnCloseSearch = document.querySelector('.button--close');
 
 const btnOpenShop = document.querySelector('.button-drawer--open');
-const shopArea = document.querySelector('.item--content__text');
+const btnOpenItem = document.querySelector('.item--open');
+const shopArea = document.querySelector('.text__item');
+const itemArea = document.querySelector('.text__wrapper');
 const shopInner = document.querySelector('.main__item--inner');
+const menuSmall = document.querySelector('.menu--small');
 
 //drawer menu
 btnDrawer.addEventListener('click', () => {
   menuDrawer.classList.add('is-visible');
-  menuMain.classList.add('is-visible');
+  contentMenu.classList.add('is-visible');
 });
 
 btnClose.addEventListener('click', () => {
   menuDrawer.classList.remove('is-visible');
-  menuMain.classList.remove('is-visible');
+  contentMenu.classList.remove('is-visible');
 });
 
 shopArea.addEventListener('click', () => {
   btnOpenShop.classList.toggle('is-hidden');
   shopInner.classList.toggle('is-visible');  
 });
+
+itemArea.addEventListener('click', () => {
+  btnOpenItem.classList.toggle('is-hidden');
+  menuSmall.classList.toggle('is-visible');
+})
 
 //search
 btnOpenSearch.addEventListener('click', () => {
@@ -42,20 +50,6 @@ btnCloseSearch.addEventListener('click', () => {
 // slide show
 
 var swiper = new Swiper(".main__item--swiper", {
-  // slidesPerView: 1,
-  // spaceBetween: 0,
-  // slidesPerGroup: 1,
-  // loop: true,
-  // loopFillGroupWithBlank: true,
-
-  // autoplay: {
-  //   delay: 3000,
-  // },
-  
-  // pagination: {
-  //   el: ".swiper-pagination",
-  // },
-
   slidesPerView: 1,
   spaceBetween: 0,
   pagination: {
