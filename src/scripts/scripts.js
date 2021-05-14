b7036815df92a4a04b97eafa8936ceb3f9b41e09
binhdao29 <binhchan0293@gmail.com>
2021-05-14 12:32:13 +0700

    mega menu comlete

--- src/scripts/scripts.js
+++ src/scripts/scripts.js
@@ -1,4 +1,4 @@
-const btnDrawer = document.querySelector('.button__drawer');
+const btnDrawer = document.querySelector('.button--open');
 const menuDrawer = document.querySelector('.menu__drawer');
 const btnClose = document.querySelector('.button-close');
 const contentMenu = document.querySelector('.content');
@@ -7,13 +7,15 @@ const menuTransparent = document.querySelector('.header--transparent');
 const btnOpenSearch = document.querySelector('.button--search');
 const formSearch = document.querySelector('.search');
 const btnCloseSearch = document.querySelector('.button--close');
-
+const btnNavbarSearch = document.querySelector('.item--search');
+//toggle menu
 const btnOpenShop = document.querySelector('.button-drawer--open');
-const btnOpenItem = document.querySelector('.item--open');
 const shopArea = document.querySelector('.text__item');
-const itemArea = document.querySelector('.text__wrapper');
+// toggle menu-small
 const shopInner = document.querySelector('.main__item--inner');
-const menuSmall = document.querySelector('.menu--small');
+const btnOpenItem = document.getElementsByClassName('item--open');
+const itemArea = document.getElementsByClassName('text__wrapper');
+const menuSmall = document.getElementsByClassName('menu--small');
 
 //drawer menu
 btnDrawer.addEventListener('click', () => {
@@ -31,10 +33,27 @@ shopArea.addEventListener('click', () => {
   shopInner.classList.toggle('is-visible');  
 });
 
-itemArea.addEventListener('click', () => {
-  btnOpenItem.classList.toggle('is-hidden');
-  menuSmall.classList.toggle('is-visible');
-})
+
+//open small menu
+
+const toggleSmallMenu = () => {
+  for (let i = 0; i < 3; i++) {
+    itemArea[i].addEventListener('click', () => {
+      for (let j = 0; j < 3; j++) {
+        if (j !== i) {
+          btnOpenItem[j].classList.remove('is-hidden');
+          menuSmall[j].classList.remove('is-visible');
+        }
+      }
+
+      btnOpenItem[i].classList.toggle('is-hidden');
+      menuSmall[i].classList.toggle('is-visible');
+    });
+  }
+}
+
+toggleSmallMenu();
+
 
 //search
 btnOpenSearch.addEventListener('click', () => {
@@ -43,18 +62,20 @@ btnOpenSearch.addEventListener('click', () => {
   formSearch.classList.toggle('is-visible');
 });
 
+btnNavbarSearch.addEventListener('click', () => {
+  formSearch.classList.toggle('is-visible');
+})
+
 btnCloseSearch.addEventListener('click', () => {
   formSearch.classList.toggle('is-visible');
 });
 
 // slide show
 
-var swiper = new Swiper(".main__item--swiper", {
+const swiper = new Swiper(".main__item--swiper", {
   slidesPerView: 1,
-  spaceBetween: 0,
   pagination: {
     el: ".swiper-pagination",
-    clickable: true
+    clickable: true,
   }
-  
 });
